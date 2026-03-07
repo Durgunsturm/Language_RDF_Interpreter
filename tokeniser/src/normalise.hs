df2615eb7570698a9d56de2e6dac3a4dbd91b3c4
sp3g24 <samir.p1@virginmedia.com>
2026-03-07 02:55:58 +0000

    Initialised normaliser
    Added basic file IO, need to store tokens from ttl file next

--- tokeniser/src/normalise.hs
+++ tokeniser/src/normalise.hs
@@ -1,2 +1,17 @@
+module Normalise (
+    normalise
+) where
+
 --Translates turtle abbrieviations into standard form.
 --File is required to be normalised fully before querying.
+
+
+--Translates an abbreviated ttl file into a normalised ttl file
+normalise :: FilePath -> FilePath -> IO ()
+normalise input output = do
+  contents <- readFile input
+  writeFile output contents
+
+--Passes test files automatically to normalise
+autoNormal :: IO ()
+autoNormal = normalise "normTest/normInput.ttl" "normTest/normOutput.ttl"
