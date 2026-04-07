3659536ea1d88131e37c5cbc2beca630f98bd823
STELIRON14\lstel <leo@steliaros.com>
2026-04-07 16:31:49 +0100

    Added comments to Main and made minor changes

--- tokeniser/app/Main.hs
+++ tokeniser/app/Main.hs
@@ -35,18 +35,18 @@ noParse e = do let err =  show e
 
 runProgram :: [Expr] -> IO()
 runProgram exprs = do
-    let env = buildEnv exprs -- build environment from expressions
+    let env = buildEnv exprs -- from expressions, link variable name to file path
     dataset <- loadDataset exprs env -- create dataset from environment
     processQueries exprs env dataset -- process queries
 
 buildEnv :: [Expr] -> Env
 buildEnv [] = [] -- base case
 buildEnv (Variable varName rdfTerm : xs) = -- graph variables extracted from expression list and path to file linked to variable name in environment
-    let path = extractPath rdfTerm
-    in (varName, path) : buildEnv xs
+    let path = extractPath rdfTerm -- will only get valid file path if rdfTerm is LitStr
+    in (varName, path) : buildEnv xs -- recursively build list of graph variable names and associated file path
 buildEnv (_:xs) = buildEnv xs -- skip expressions that aren't variables
 
--- convert file path from RDFTerm into string which can be read
+-- convert file path from RDFTerm into string, only LitStr RDFTerms produce usable file paths
 extractPath :: RDFTerm -> String
 extractPath (LitStr s) = s
 extractPath (URI _) = ""
@@ -55,20 +55,20 @@ extractPath (LitInt _) = ""
 -- normalise RDF graphs in dataset
 loadDataset :: [Expr] -> Env -> IO [(String,[Triple])]
 loadDataset [] _ = return [] -- base case
-loadDataset (Norm targetVar inVar outVar : xs) env = do
-    case (lookup inVar env, lookup outVar env) of
-        (Just inPath, Just outPath) -> do -- if input file path and output file path in environment
-            normalise inPath outPath -- normalie input into output
-            rdfData <- readFile outPath -- read normalised file contents
-            let triples = parseRDF rdfData -- convert string into triples data type
+loadDataset (Norm targetVar inVar normVar : xs) env = do
+    case (lookup inVar env, lookup normVar env) of
+        (Just inPath, Just normPath) -> do -- if input file path and normalise file path in environment
+            normalise inPath normPath -- normalise input into normalised file
+            rdfData <- readFile normPath -- read normalised file contents
+            let triples = parseRDF rdfData -- convert rdf graph string into triples data type
             rest <- loadDataset xs env -- repeat on rest of Query
             return ((targetVar, triples) : rest) -- return list of target variable and associated triples as dataset for executeQuery
-        _ -> error ("Undefined variable in Norm command: missing assignment for " ++ inVar ++ " or " ++ outVar)
-loadDataset (_ : xs) env = loadDataset xs env -- for all elements of Query that aren't (Norm target in out)
+        _ -> error "Variable in 'init' not assigned"
+loadDataset (_ : xs) env = loadDataset xs env -- for all elements of Query that aren't of form (Norm target input normalise)
 
 processQueries :: [Expr] -> Env -> Dataset -> IO ()
 processQueries [] _ _ = return () -- base case
-processQueries (Queries q : xs) env dataset = do
+processQueries (Queries q : xs) env dataset = do -- Expressions matching Query pattern
     -- for testing
     putStrLn "Dataset contents:"
     print dataset
@@ -80,29 +80,30 @@ processQueries (Queries q : xs) env dataset = do
         resultStr = unparseRDF resultTriples -- unparse query result into string using unparseRDF
         (fromVars, toVars) = getFromTo q -- get from file path and to file path
         isConsoleOutput = case toVars of
-            Just toVar -> False
-            Nothing -> True
+            Just toVar -> False -- case when TO clause not included in program
+            Nothing -> True -- case when TO clause included in program
     if isConsoleOutput then do -- write to console
         putStrLn "Query Result:"
         putStrLn resultStr
-    else do -- write to to file
-        let outVar = case toVars of
+    else do -- write to file
+        let outVar = case toVars of -- extract head of toVars into outVar (removing this and instead using 'outVar = head toVars' causes a syntax error)
                 Just vars -> head vars -- filepath
                 Nothing -> "" -- shouldn't ever occur as Nothing toVars already handled in isConsoleOutput
         case lookup outVar env of
             Just outPath -> do -- output path exists, write to output path
                 writeFile outPath resultStr
-                putStrLn ("Query Result written to: " ++ outPath)
+                putStrLn ("Query Result written to: " ++ outPath) -- output to console informing user which file has been written to
             Nothing -> do -- else write to console and tell user failed to write to file
                 putStrLn ("Failed to write to: " ++ outVar ++ "Printing to console:")
-                putStrLn resultStr
-    processQueries xs env dataset
+                putStrLn resultStr -- output result to console
+    processQueries xs env dataset -- recursively call on rest of expressions list
 processQueries (_:xs) env dataset = processQueries xs env dataset -- if expression isn't a query, recursively call on list of expressions
 
 -- get from file path and to file path, only Select statements contain file paths
 getFromTo :: Query -> ([String], Maybe [String])
-getFromTo (Select _ from (Just to) _) = (from, Just to)
-getFromTo (Select _ from Nothing _) = (from, Nothing)
+getFromTo (Select _ from (Just to) _) = (from, Just to) -- TO clause exists in program
+getFromTo (Select _ from Nothing _) = (from, Nothing) -- TO clause doesn't exist in program
+-- TO clause is only defined in SELECT statements
 getFromTo (Union q1 _) = getFromTo q1
 getFromTo (Group q1 _) = getFromTo q1
 getFromTo (Inter q1 _) = getFromTo q1
